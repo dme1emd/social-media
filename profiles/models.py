8e99593f6200da04955c1fc097f589242e2054c6
mac <macbook@mohamed-2.local>
2022-07-06 23:02:03 +0100

    fixed some bugs

--- profiles/models.py
+++ profiles/models.py
@@ -5,21 +5,21 @@ from django.contrib.auth.models import BaseUserManager , AbstractBaseUser , Perm
 from django.forms import EmailField
 # Create your models here.
 class ProfileManager(BaseUserManager):
-    def create_user(self  , username , bio, password = None) : 
-        user = self.model(username=username , bio = bio)
+    def create_user(self  , username,password = None) : 
+        user = self.model(username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    def create_superuser(self , username , bio , password = None) :
-        user = self.create_user( username , bio , password)
+    def create_superuser(self , username , password = None) :
+        user = self.create_user( username =username,password= password)
         user.is_superuser = True
         user.save(using=self._db)
         return user
 class Profile(PermissionsMixin , AbstractBaseUser):
     username = models.CharField(max_length=30 , unique=True)
-    bio = models.CharField(max_length=150)
-    profile_pic = models.ImageField(upload_to='profile_pic')
-    is_staff = models.BooleanField(default=False)
+    bio = models.CharField(max_length=150 , blank=True , null=True)
+    profile_pic = models.ImageField(upload_to='profile_pic', blank=True , null=True)
+    is_staff = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     objects= ProfileManager()
     USERNAME_FIELD = 'username'
